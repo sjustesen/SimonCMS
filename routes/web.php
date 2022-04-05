e29b9c3c53547eda4c8f0aaa55566f83a59937ed
Simon Justesen <simon.justesen@gmail.com>
2022-04-05 09:35:03 +0200

    Routing updates. Ditching client side routes as they're generally too fragile, I think.

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,8 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('login',  ['uses' => 'AuthController@login']);
     $router->get('logout',  ['uses' => 'AuthController@logout']);
     
+
+
     // content api routes
     $router->get('content',  ['uses' => 'Api\ContentApiController@listContent']);
     $router->get('content/{id}', ['uses' => 'Api\ContentApiController@showContent']);
@@ -79,19 +81,19 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->delete('media/{id}', ['uses' => 'Api\MediaApiController@deleteMedia']);
     $router->put('media/{id}', ['uses' => 'Api\MediaApiController@updateMedia']);
 
-    $router->get('doctype',  ['uses' => 'Api\DoctypeApiController@listDoctypes']);
+    $router->get('doctypes',  ['uses' => 'Api\DoctypeApiController@listDoctypes']);
     $router->get('doctype/{id}', ['uses' => 'Api\DoctypeApiController@showDoctype']);
     $router->post('doctype', ['uses' => 'Api\octypeApiController@createDoctype']);
     $router->delete('doctype/{id}', ['uses' => 'Api\DoctypeApiController@deleteDoctype']);
     $router->put('doctype/{id}', ['uses' => 'Api\DoctypeApiController@updateDoctype']);
 
-    $router->get('datatype',  ['uses' => 'Api\ApiController@listDatatypes']);
+    $router->get('datatypes',  ['uses' => 'Api\ApiController@listDatatypes']);
     $router->get('datatype/{id}', ['uses' => 'Api\ApiController@showDatatype']);
     $router->post('datatype', ['uses' => 'Api\ApiController@createDatatype']);
     $router->delete('datatype/{id}', ['uses' => 'Api\ApiController@deleteDatatype']);
     $router->put('datatype/{id}', ['uses' => 'Api\ApiController@updateDatatype']);
 
-    $router->get('template',  ['uses' => 'Api\ApiController@listTemplates']);
+    $router->get('templates',  ['uses' => 'Api\ApiController@listTemplates']);
     $router->get('template/{id}', ['uses' => 'Api\ApiController@showTemplate']);
     $router->post('template', ['uses' => 'Api\ApiController@createTemplate']);
     $router->delete('template/{id}', ['uses' => 'Api\ApiController@deleteTemplate']);
