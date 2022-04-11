3c1620c8dcbc050a8a19423067a632f9e201e494
Simon Justesen <simon.justesen@gmail.com>
2022-04-11 10:51:22 +0200

    AdminNavTreeController changes

--- app/Http/Controllers/AdminNavtreeController.php
+++ app/Http/Controllers/AdminNavtreeController.php
@@ -40,4 +40,26 @@ class AdminNavtreeController extends Controller
         
         return json_encode($menu_array);
     }
+
+    public function media() {
+        $menu_array = [];
+        $item_one = new \StdClass();
+        $item_one->title = 'This tree should';
+        $item_one->icon = '';
+        $item_one->href='/admin';
+
+        $item_two = new \StdClass();
+        $item_two->title = 'Display';
+        $item_two->icon = '';
+        $item_two->href='/admin/media';
+
+        $item_three = new \StdClass();
+        $item_three->title = 'Medianodes';
+        $item_three->icon = '';
+        $item_three->href = '/admin/settings';
+
+        array_push($menu_array, $item_one, $item_two, $item_three);
+        
+        return json_encode($menu_array);
+    }
 }
