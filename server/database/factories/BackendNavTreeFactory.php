180ba7bb803eda6018eab52945d0bc56ba5fcd1f
Simon Justesen <simon.justesen@gmail.com>
2022-04-14 10:18:33 +0200

    Toying with providers

---
+++ server/database/factories/BackendNavTreeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BackendNavTree;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BackendNavtreeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = BackendNavTree::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+        ];
+    }
+}
