a7990a11738ac99c317bd4179de633d8afc132d5
Simon Justesen <simon.justesen@gmail.com>
2022-04-18 01:09:41 +0200

    Update

--- server/resources/views/admin/settings/doctypes/index.blade.php
+++ server/resources/views/admin/settings/doctypes/index.blade.php
@@ -1,34 +1,49 @@
 @extends('admin.layout.master')
 @section('content')
 <main>
-	<h1>Create New DocumentType</h1>
 	<simoncms-doctypeeditor>
 		<slot>
-			<form>
-				<button class="uk-button uk-button-primary" id="btnSaveDoctype">Save</button>
-				<div class="uk-container uk-width-1-1">
-					<label>Name</label>
-					<input class="uk-input uk-form-large" type="text" placeholder="100">
-					<label>Alias</label>
-					<input class="uk-input uk-form-large" type="text" placeholder="100">
-					
-					<h2>Controls</h2>					
+			<form class="">
+				<div class="uk-container">
+				<h1>Create New DocumentType</h1>
+
+					<button class="uk-button uk-button-primary" id="btnSaveDoctype">Save</button>
+
+					<div class="uk-width-1-2">
+						<label>Name</label>
+						<input class="uk-input uk-form-large" type="text" placeholder="100">
+					</div>
+					<div class="uk-width-1-2">
+						<label>Alias</label>
+						<input class="uk-input uk-form-large" type="text" placeholder="100">
+					</div>
+				</div>
+
+				<div class="uk-container uk-margin">
+					<h2>Controls</h2>
+
 					<ul uk-tab id="dtTabs">
-						<li><a href="#">Item</a></li>
-						<li><a href="#">Item</a></li>
-						<li class="uk-active"><a href="#">Generic Properties</a></li>
-						<li><button id="btnAddTab" class="uk-button uk-button-small uk-button-primary">Add tab</button></li>
+						<li class="uk-active" data-nonremovable><a href="#">Generic Properties</a></li>
+						<li><button id="btnAddTab" class="uk-button uk-button-small uk-button-primary" data-nonremovable>Add tab</button></li>
 					</ul>
-					<input class="uk-input uk-form-large" type="text" placeholder="Visual name">
-					<input class="uk-input uk-form-large" type="text" placeholder="Data alias">
-					
-					<select class="uk-select" id="dtComponents">
-						<option>Select Doctype control</option>
-						<option value="text">Text</option>
-						<option value="textarea">Textarea</option>
-						<option value="number">Number</option>
-					</select>
-							
+
+					<button id="btnAddField" class="uk-button uk-button-small uk-button-primary">Add field</button>
+
+					<div id="newfields_template">
+						<input class="uk-input uk-form-large" type="text" placeholder="Visual name">
+						<input class="uk-input uk-form-large" type="text" placeholder="Data alias">
+
+						<select class="uk-select" class="dtComponents">
+							<option>Select Doctype control</option>
+							<option value="text">Text</option>
+							<option value="textarea">Textarea</option>
+							<option value="number">Number</option>
+						</select>
+					</div>
+					<div id="newfields_container"  uk-margin>
+
+					</div>
+
 				</div>
 			</form>
 		</slot>
