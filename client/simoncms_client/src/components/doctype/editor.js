573323b5274d4c9c2f2549c955492201209775e5
Simon Justesen <simon.justesen@gmail.com>
2022-04-18 10:20:21 +0200

    Doctype Editor: Keyup event for creating a new tab

--- client/simoncms_client/src/components/doctype/editor.js
+++ client/simoncms_client/src/components/doctype/editor.js
@@ -5,37 +5,54 @@ export default class DoctypeEditor extends LitElement {
         super();
     }
 
-    connectedCallback() {
-        super.connectedCallback();
+    registerEventListeners() {
+
+        // event for the save-button
+        
         document.querySelector('#btnSaveDoctype').addEventListener('click', (e) => {
             e.preventDefault();
             console.dir('Doctype saving...')
         })
 
+         // event for the addfield-button
+       
         document.querySelector('#btnAddField').addEventListener('click', (e) => {
             e.preventDefault();
             const clonedNodes = document.querySelector('#newfields_template').cloneNode(true);
             document.querySelector('#newfields_container').appendChild(clonedNodes);
         });
 
+        // event for adding a new tab
+
         document.querySelector('#btnAddTab').addEventListener('click', (e) => {
             e.preventDefault();
+
             let doctypeTabArray = document.querySelector('#dtTabs');
             let listElement = document.createElement('li');
             listElement.setAttribute('class', 'newtab')
             
-            let anchorElement = document.createElement('input');
-            anchorElement.type = 'text';
-            anchorElement.setAttribute('class', 'uk-input uk-form-small');
-            anchorElement.addEventListener('keyup', (e) => {
+            let newTabElement = document.createElement('input');
+            newTabElement.type = 'text';
+            newTabElement.placeholder = 'Tab name';
+            newTabElement.setAttribute('class', 'uk-input uk-form-small');
+            
+            newTabElement.addEventListener('keyup', (e) => {
                 e.preventDefault();
-                if (e.key == 'Enter')
-                    console.log(e)
+                if (e.key == 'Enter') {
+                   listElement.innerHTML = `<a href="#">${e.currentTarget.value}</a>`
+                   newTabElement.setAttribute('style', 'display: none;');
+                  }
             });
 
-            listElement.appendChild(anchorElement);
+            listElement.remove(newTabElement);
+            listElement.appendChild(newTabElement);
             doctypeTabArray.prepend(listElement);
         })
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.registerEventListeners();
     }
 
     render() {
--- server/resources/views/admin/settings/templates/index.blade.php
+++ server/resources/views/admin/settings/templates/index.blade.php
@@ -1,4 +1,12 @@
-@extends('admin.layout.master')
+@extends('admin.layout.3col')
+
+@section('sectionnav')
+<small>Navigation</small>
+<sc-templatetree>
+    <slot></slot>
+</sc-templatetree>
+@endsection
+
 @section('content')
 <h1>Templates</h1>
 @endsection
